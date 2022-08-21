f2d88a35d387a406a53b855f6d803e45ae680e9a
Marcelo Kopmann <marcelokopmann@gmail.com>
2022-08-21 15:54:08 -0300

    refac: make card user smaller

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -10,23 +10,20 @@ export function UserCard ({ user, space, date }: Props) {
   const userSpace = user.spacesByDate[date][space]
 
   return (
-    <div className='card w-96 bg-base-100 shadow-xl'>
-      <figure><img src={`https://robohash.org/${user.gatherPlayerId}`} alt={user.gatherName} /></figure>
-      <div className='card-body'>
-        <h2 className='card-title'>
+    <div className='card w-full bg-base-100 shadow-xl'>
+      <figure><img src={`https://robohash.org/${user.gatherPlayerId}`} alt={user.gatherName} className='h-20 object-cover' /></figure>
+      <div className='card-body text-sm'>
+        <h2 className='card-title text-sm'>
           {user.gatherName}
           {userSpace.isOnline && (
             <div className='badge bg-green-600'>ONLINE</div>
           )}
         </h2>
         <ul>
-          <li>Passos: {userSpace.steps}</li>
-          <li>
-            Interações: {userSpace.interactions}{' '}
-            {`(${userSpace.objectInteractions?.length} objetos)`}
-          </li>
-          <li>Mensagens: {userSpace.messages}</li>
-          <li>Tempo Online: {userSpace.timeOnlineInMinutes?.toFixed(0)} minutos</li>
+          <li>👣 {userSpace.steps}</li>
+          <li>🤏 {userSpace.interactions}</li>
+          <li>💬 {userSpace.messages}</li>
+          <li>🕒 {userSpace.timeOnlineInMinutes?.toFixed(0)} min</li>
         </ul>
       </div>
     </div>
--- src/pages/users.tsx
+++ src/pages/users.tsx
@@ -22,7 +22,7 @@ export default function HomePage () {
           {spaces.map((space) => (
             <div key={space}>
               {space}
-              <div className='grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3'>
+              <div className='m-2 grid grid-cols-3 gap-2 lg:grid-cols-6'>
                 {users &&
                   users
                     .filter((user) => {
