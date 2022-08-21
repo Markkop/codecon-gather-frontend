e524f0846136f47f048e8eecde9a91afc187446d
Marcelo Kopmann <marcelokopmann@gmail.com>
2022-08-21 13:31:38 -0300

    feat: add space images

--- src/components/AllTimeSpacesStats.tsx
+++ src/components/AllTimeSpacesStats.tsx
@@ -3,6 +3,11 @@ import { getAllTimeSpaceStats } from '../utilities/user'
 
 const spaceImage = {
   'codecon-2022-sala-1': 'https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/DfVMRgRjpjJSoK5y/8IQHEaoJOpogX22xI30T8U',
+  'codecon-2022-sala-2': 'https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/aqQVlXTpX2ISp0ci/vKwJlXWj9VOLR4YIxUUIbP',
+  'codecon-2022-sala-3': 'https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/ZXHxMgrgOseuus8N/kD40z9BtpsJwb3IPIR7uND',
+  'codecon-2022-happyhour-1': 'https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/mWkpadGDx6dBTlDv/HTTtAn4ezeHLXed9EB5PGH',
+  'codecon-2022-happyhour-2': 'https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/aL93ZLLie7531JYj/37qNMI9JIkPwNCvN6ftOUy',
+  'Codecon-2022': 'https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/VSqg1CcrGZHUwtaT/NLRDMJxcjLIUVic4XZFxeU',
   default: 'https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/DfVMRgRjpjJSoK5y/8IQHEaoJOpogX22xI30T8U'
 }
 
