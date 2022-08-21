c828c77d54dc6860fa7a30da2b1193cc89115c42
Marcelo Kopmann <marcelokopmann@gmail.com>
2022-08-21 13:24:13 -0300

    fix: remove unused imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,8 @@
 import { useEffect, useState } from 'react'
 import { AllTimeSpacesStats } from '../components/AllTimeSpacesStats'
 import { StandStats } from '../components/StandsStats'
-import { UserCard } from '../components/UserCard'
 import { User } from '../types/user'
 import { getUsers } from '../utilities/api'
-import { getDates, getSpacesNames } from '../utilities/user'
 
 export default function HomePage () {
   const [users, setUsers] = useState<User[]>([])
