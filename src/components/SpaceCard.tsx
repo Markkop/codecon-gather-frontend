49e5e555dfaddd3c4358f15e22f5f1b4f364b22a
Marcelo Kopmann <marcelokopmann@gmail.com>
2022-08-21 18:23:54 -0300

    feat: collapse spaces and dates section on users page

--- src/components/SpaceCard.tsx
+++ src/components/SpaceCard.tsx
@@ -1,21 +1,4 @@
-const spaceImage = {
-  'codecon-2022-sala-1': 'https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/DfVMRgRjpjJSoK5y/8IQHEaoJOpogX22xI30T8U',
-  'codecon-2022-sala-2': 'https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/aqQVlXTpX2ISp0ci/vKwJlXWj9VOLR4YIxUUIbP',
-  'codecon-2022-sala-3': 'https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/ZXHxMgrgOseuus8N/kD40z9BtpsJwb3IPIR7uND',
-  'codecon-2022-happyhour-1': 'https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/mWkpadGDx6dBTlDv/HTTtAn4ezeHLXed9EB5PGH',
-  'codecon-2022-happyhour-2': 'https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/aL93ZLLie7531JYj/37qNMI9JIkPwNCvN6ftOUy',
-  'Codecon-2022': 'https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/VSqg1CcrGZHUwtaT/NLRDMJxcjLIUVic4XZFxeU',
-  default: 'https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/uploads/DfVMRgRjpjJSoK5y/8IQHEaoJOpogX22xI30T8U'
-}
-
-const friendlySpaceName = {
-  'codecon-2022-sala-1': 'Sala 1',
-  'codecon-2022-sala-2': 'Sala 2',
-  'codecon-2022-sala-3': 'Sala 3',
-  'codecon-2022-happyhour-1': 'HH 1',
-  'codecon-2022-happyhour-2': 'HH 1',
-  'Codecon-2022': 'Intro'
-}
+import { friendlySpaceName, spaceImage } from '../data/spaces'
 
 type Space = {
   spaceName: string,
--- src/pages/users.tsx
+++ src/pages/users.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { UserCard } from '../components/UserCard'
+import { friendlySpaceName } from '../data/spaces'
 import { User } from '../types/user'
 import { getUsers } from '../utilities/api'
 import { getDates, getSpacesNames } from '../utilities/user'
@@ -12,29 +13,40 @@ export default function HomePage () {
   }, [])
 
   const spaces = getSpacesNames(users)
-  const dates = getDates(users)
+  const dates = getDates(users).sort((a: string, b: string) => {
+    return Date.parse(b.split('/').reverse().join('/')) - Date.parse(a.split('/').reverse().join('/'))
+  })
 
   return (
     <>
       {dates.map(date => (
-        <div key={date}>
-          {date}
-          {spaces.map((space) => (
-            <div key={space}>
-              {space}
-              <div className='m-2 grid grid-cols-3 gap-2 lg:grid-cols-6'>
-                {users &&
+        <div className='collapse' key={date}>
+          <input type='checkbox' />
+          <div className='collapse-title text-xl font-medium'>
+            {date} 🔽
+          </div>
+          <div className='collapse-content'>
+            {spaces.map((space) => (
+              <div className='collapse' key={space}>
+                <input type='checkbox' />
+                <div className='collapse-title text-xl font-medium'>
+                  {friendlySpaceName[space]} 🔽
+                </div>
+                <div className='collapse-content'>
+                  <div className='m-2 grid grid-cols-3 gap-2 lg:grid-cols-6'>
+                    {users &&
                   users
                     .filter((user) => {
                       return user.spacesByDate && user.spacesByDate[date][space]
                     })
                     .map((user) => <UserCard user={user} space={space} date={date} key={user.gatherPlayerId} />)}
+                  </div>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      )
-      )}
+      ))}
     </>
   )
 }
