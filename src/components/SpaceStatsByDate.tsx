2bbf6d5034e63625ab07d0df9124629923a8be43
Marcelo Kopmann <marcelokopmann@gmail.com>
2022-08-21 18:56:48 -0300

    feat: add date selector

--- src/components/SpaceStatsByDate.tsx
+++ src/components/SpaceStatsByDate.tsx
@@ -7,16 +7,11 @@ type SpaceStatsByDateProps = {
   date: string
 }
 
-function getTodayDate () {
-  const todayDateString = new Date(Date.now()).toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo' })
-  return todayDateString.split(' ')[0]
-}
-
 export function SpaceStatsByDate ({ users, date }: SpaceStatsByDateProps) {
-  const allTimeSpaceStats = getSpaceStatsByDate(users, date || getTodayDate())
+  const allTimeSpaceStats = getSpaceStatsByDate(users, date)
   return (
     <section>
-      <h1>Stats for {date || getTodayDate()}</h1>
+      <h1>Stats for {date}</h1>
       <div className='m-2 grid grid-cols-1 gap-2 sm:grid-cols-3 lg:grid-cols-6'>
         {allTimeSpaceStats.map(space => <SpaceCard space={space} key={space.spaceName} />)}
       </div>
--- src/pages/byDate/[[...date]].tsx
+++ src/pages/byDate/[[...date]].tsx
@@ -6,10 +6,21 @@ import { StandStats } from '../../components/StandsStats'
 import { User } from '../../types/user'
 import { getUsers } from '../../utilities/api'
 import { isValidDate } from '../../utilities/dates'
+import 'react-datepicker/dist/react-datepicker.css'
+import DatePicker, { registerLocale } from 'react-datepicker'
+import ptBR from 'date-fns/locale/pt-BR'
+import { routes } from '../../components/Header'
+registerLocale('pt-BR', ptBR)
+
+function parseDate (date: Date) {
+  const todayDateString = new Date(date).toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo' })
+  return todayDateString.split(' ')[0]
+}
 
 export default function ByDatePage () {
   const [users, setUsers] = useState<User[]>([])
-  const [date, setDate] = useState(null)
+  const [rawDate, setRawDate] = useState(new Date())
+  const [date, setDate] = useState(parseDate(new Date()))
 
   const router = useRouter()
   const queryParamDate = String(router.query?.date).replace(/-|\./g, '/')
@@ -17,6 +28,7 @@ export default function ByDatePage () {
   useEffect(() => {
     if (!isValidDate(queryParamDate)) return
     setDate(queryParamDate)
+    setRawDate(new Date(Date.parse(queryParamDate.split('/').reverse().join('/'))))
   }, [queryParamDate])
 
   useEffect(() => {
@@ -25,6 +37,21 @@ export default function ByDatePage () {
 
   return (
     <>
+      <div className='flex'>
+        <div className='mx-auto justify-center'>
+          <DatePicker
+            selected={rawDate}
+            onChange={(date:Date) => {
+              setRawDate(date)
+              setDate(parseDate(date))
+              router.push({ pathname: `${routes.convert.path}/${parseDate(date).replace(/\//g, '-')}` }, undefined, { shallow: true })
+            }}
+            dateFormat='P'
+            locale='pt-BR'
+            className='text-center'
+          />
+        </div>
+      </div>
       <SpaceStatsByDate users={users} date={date} />
       <StandStats users={users} date={date} />
     </>
