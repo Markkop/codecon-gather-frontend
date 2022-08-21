0559a0a45a56f7e38f694ac3d9492ae809ac5993
Marcelo Kopmann <marcelokopmann@gmail.com>
2022-08-21 18:25:00 -0300

    feat: change title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ function MyApp ({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>NextJS TailwindCSS TypeScript Starter</title>
+        <title>Codecon Gather Stats </title>
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       </Head>
       <Header />
